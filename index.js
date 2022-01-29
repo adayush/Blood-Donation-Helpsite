89676bcc40fa220fbc7679cc44984bbf1a355ddf
adayush <adayush0@gmail.com>
2022-01-29 15:28:45 +0530

    Added package.json, public dir and gitignore

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { readFile } = require('fs').promises;
+require('dotenv').config()
+var path = require('path')
+
+const app = express();
+
+
+// To serve static files. Put all static files in public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (request, response) => {
+
+//     readFile('./index.html', 'utf8', (err, html) => {
+//         if (err) {
+//             response.status(500).send('couldnt fetch html');
+//         }
+//         response.send(html);
+
+//     })
+// });
+
+// Using promises. Benefitial if having multiple async ops to handle in single request
+app.get('/', async (request, response) => {
+
+    response.send( await readFile('./index.html', 'utf8') );
+
+    readFile('./index.html', 'utf8', (err, html) => {
+        if (err) {
+            response.status(500).send('couldnt fetch html');
+        }
+        response.send(html);
+
+    })
+});
+
+
+app.listen(process.env.PORT, () => console.log(`App available on ${process.env.PORT}`));
---
+++ public/scripts/script.js
@@ -0,0 +1,18 @@
+/*
+const fs = require('fs');
+
+
+// fs.writeFileSync("Raj.txt", JSON.stringify(Raj), function(err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
+
+fs.readFile('Raj.txt', 'utf-8', (err, data) => {
+    if(err) {
+        throw err;
+    }
+
+    console.log(JSON.parse(data));
+})
+*/
